2bb0931b96bbd49a1093eaaf798427df8542ea8e
Manik Sharma <maniksharma.rke@gmail.com>
2024-09-14 23:10:59 +0530

    filed added

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ app.use('/sales', salesRoutes)
 
 
 /* MONGOOSE CONFIG */
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.MONGODB_URI
 const PORT = process.env.PORT || 3001
 mongoose
   .connect(DATABASE_URL as string)
